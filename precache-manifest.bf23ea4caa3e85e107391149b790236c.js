ca5d8cdd03453261859d1774ba183f06271ea0a7
Nic Landaverde <nicodemus.landaverde98@gmail.com>
2020-06-08 13:58:09 -0400

    logger
    initial

---
+++ precache-manifest.bf23ea4caa3e85e107391149b790236c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d19ef35e03d56a41a1d8ef302e45431",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ee45559c94855a9f63a",
+    "url": "/static/css/main.34ca8a15.chunk.css"
+  },
+  {
+    "revision": "25d0072a50e57da4f18e",
+    "url": "/static/js/2.90530fb8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.90530fb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ee45559c94855a9f63a",
+    "url": "/static/js/main.02b9d604.chunk.js"
+  },
+  {
+    "revision": "751fe6a8fad62c25e8fa",
+    "url": "/static/js/runtime-main.417c6eb4.js"
+  }
+]);
